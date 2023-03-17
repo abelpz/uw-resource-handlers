e6fb19a97e57d0904262d05bc6d6c91def451d54
Abel Perez <abelper54@gmail.com>
2023-03-17 13:29:08 -0400

    added tsv-groupdata-parser

---
+++ libs/tsv-parser/README.md
@@ -0,0 +1,11 @@
+# tsv-parser
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build tsv-parser` to build the library.
+
+## Running unit tests
+
+Run `nx test tsv-parser` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/tsv-parser/jest.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+module.exports = {
+  displayName: 'tsv-parser',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/tsv-parser',
+};
